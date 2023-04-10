090b7a04dd0c2e55d35276983be1aa6e06831335
Onyu <dideoduf1@gmail.com>
2023-04-10 13:03:08 +0900

    feat: 회원가입구현

--- src/main/java/controller/user/JoinTaskController.java
+++ src/main/java/controller/user/JoinTaskController.java
@@ -1,51 +1,47 @@
 package controller.user;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mindrot.jbcrypt.BCrypt;
 
-import data.User;
-
-@WebServlet("/user/login-task")
-public class LoginTaskController extends HttpServlet{
+@WebServlet("/user/join-task")
+public class JoinTaskController extends HttpServlet{
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.setCharacterEncoding("utf-8");
+		
 		String id = req.getParameter("id");
 		String pass = req.getParameter("pass");
-		
+		String nick = req.getParameter("nick");
+
 		SqlSessionFactory factory= 
 				(SqlSessionFactory)req.getServletContext().getAttribute("sqlSessionFactory");
 		SqlSession sqlSession = factory.openSession(true);
 		
-		User user = sqlSession.selectOne("users.findByUser", id);
-		sqlSession.close();
-		if(user != null) {
-			
-			if(id.equals(user.getId()) && BCrypt.checkpw(pass, user.getPass())) {
-				req.getSession().setAttribute("logon", true);
-				
-				HttpSession session = req.getSession();
-				session.setAttribute("logonUser", user);
-				
-				resp.sendRedirect("/");
-			}else {
-				resp.sendRedirect("/user/login?error=r");
-//				req.getRequestDispatcher("/WEB-INF/views/user/login.jsp?error=r").forward(req, resp);
-			}
-			
+		String bcPass = BCrypt.hashpw(pass, BCrypt.gensalt());
+		
+		Map<String, Object> params = new HashMap<>();
+		params.put("id", id);
+		params.put("pass", bcPass);
+		params.put("nick", nick);
+		
+
+		int r = sqlSession.insert("users.create",params);
+		
+		if(r == 1) {
+			resp.sendRedirect("/user/login");
 		}else {
-			resp.sendRedirect("/user/login?error=r");
+			req.getRequestDispatcher("/WEB-INF/views/user/join.jsp?error=r").forward(req, resp);
 		}
-	
 	}
 }
--- src/main/webapp/WEB-INF/views/user/join.jsp
+++ src/main/webapp/WEB-INF/views/user/join.jsp
@@ -5,27 +5,29 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>로그인페이지</title>
+<title>회원가입</title>
 </head>
 <link rel="stylesheet" href="/resource/css/style.css?${millis }" />
 <body>
 <div class="root">
 	<div class="login-form">
-		<form action="/user/login-task" method="post" class="sign-form">
+		<form action="/user/join-task" method="post" class="sign-form">
 			<h2>로그인</h2>
 			<div style="margin: 0.4em">
 				<input type="text" placeholder="아이디" name="id" required autocomplete="off" />
 			</div>
 			<div style="margin: 0.4em">
 				<input type="password" placeholder="비밀번호" name="pass" autocomplete="off" />
-			</div>	
-			<c:if test="${param.error ne null }"><small style="color: red;">아이디 혹은 비밀번호가 일치하지 않습니다.</small></c:if>
+			</div>
 			<div style="margin: 0.4em">
-				<button type="submit" class="join-btn">로그인</button>
+				<input type="text" placeholder="닉네임" name="nick" required autocomplete="off" />
+			</div>
+			<c:if test="${param.error eq 'r' }"><small style="color: red;">회원가입에 실패하였습니다.</small></c:if>
+			<div style="margin: 0.4em">
+				<button type="submit" class="join-btn">가입</button>
 			</div>
 		</form>
 	</div>
-	아이디가 없으신가요?<a href="/user/join"><span style="color: navy;">회원가입</span></a>
 </div>
 </body>
 </html>
